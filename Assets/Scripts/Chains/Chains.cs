361f2f806e3f6339dab2882686716a855a999825
nasonkin-aa <game-akaynt@mail.ru>
2022-12-25 14:12:40 +1000

    Fix chain

--- Assets/Scripts/Chains/Chains.cs
+++ Assets/Scripts/Chains/Chains.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-public abstract class Chains : Weapon
+public class Chains : Weapon
 {
     public List<Link> links = new List<Link>();
-   
-
-    public override void LevelUp()
-    {
-        return;
-    }
-
+    public GameObject block;
+    public GameObject chain1;
+    public GameObject chain2;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +15,35 @@ public abstract class Chains : Weapon
         links.OrderBy(x => x.name).ToList();
  
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void Attack()
     {
-        
+        return;
+    }
+
+    public override void LevelUp()
+    {
+        switch (level)
+        {
+            case 0:
+                block.SetActive(true);
+                description = "link + 1";
+                break;
+            case 1:
+                chain1.SetActive(true);
+                description = "link + 1";
+                break;
+            case 2:
+                chain2.SetActive(true);
+                description = "damage + 1";
+                break;
+            default:
+                foreach (Link link in links)
+                {
+                    link.damage += 1;
+                }
+                description = "damage +1";
+                break;
+        }
+        level++;
     }
 }
--- Assets/Scripts/Chains/Link.cs
+++ Assets/Scripts/Chains/Link.cs
@@ -13,35 +13,5 @@ public class Link : Chains
         }
 
     }
-    public override void Attack()
-    {
-        return;
-    }
-
-    public override void LevelUp()
-    {
-        switch (level)
-        {
-            case 0:
-                description = "damage +2";
-                break;
-            case 1:
-                damage += 2;
-                description = "aoe size x1.5";
-                break;
-            case 2:
-                aoeSice *= 1.5f;
-                description = "delay attack -0.5";
-                break;
-            case 3:
-                delayAttack -= 0.5f;
-                description = "damage +2";
-                break;
-            default:
-                damage += 2;
-                description = "damage +2";
-                break;
-        }
-        level++;
-    }
+   
 }
