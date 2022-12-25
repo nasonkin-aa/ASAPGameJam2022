f1a9c552fa9ad59541cc06b156a0dca91d49a77e
Brentimus <Gamepopins@gmail.com>
2022-12-25 12:16:52 +1000

    Add sprite 3

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    private bool isLeft;
+    private void Start()
+    {
+        isLeft = true;
+    }
+
+    public Animator animator;
     private Vector3 _move = new Vector3();
     public Vector3 Moving
     {
@@ -16,5 +24,16 @@ public class PlayerInput : MonoBehaviour
         _move = Vector3.zero;
         _move.x = Input.GetAxis("Horizontal");
         _move.y = Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", MathF.Abs(_move.x) + MathF.Abs(_move.y));
+        if (_move.x > 0.1f && isLeft)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            isLeft = false;
+        }
+        if (_move.x < -0.1f && !isLeft)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            isLeft = true;
+        }
     }
 }
